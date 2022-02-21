a3e73bc58300bbff8e23cc5c251554841b91a701
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2022-02-21 14:58:50 -0700

    ok got the display tests working pretty good

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Display from './../Display';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Display from "./../Display";
 
-test('renders without errors with no props', async () => { });
+test("renders without errors with no props", async () => {
+  render(<Display />);
+});
 
-test('renders Show component when the button is clicked ', () => { });
+test("renders Show component when the button is clicked ", async () => {
+  render(<Display />);
+  let showDataBtn = screen.getByText(/Press to Get Show Data/i);
+  fireEvent.click(showDataBtn);
+  await screen.findByText("Stranger Things");
+});
 
-test('renders show season options matching your data when the button is clicked', () => { });
+test("renders show season options matching your data when the button is clicked", async () => {
+  render(<Display />);
+  let showDataBtn = screen.getByText(/Press to Get Show Data/i);
+  fireEvent.click(showDataBtn);
+  await screen.findByText("Stranger Things");
+
+  const seasonSelect = screen.queryAllByTestId("season-option");
+  expect(seasonSelect).toHaveLength(4);
+});
